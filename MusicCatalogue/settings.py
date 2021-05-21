a295fa522713fbdf067a953978021afd20c8d54a
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-05-21 17:04:10 -0400

    connect to ElephantSQL, add Album and Track Models

--- MusicCatalogue/settings.py
+++ MusicCatalogue/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,13 @@ WSGI_APPLICATION = 'MusicCatalogue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ijlbrymo',
+        'HOST': 'queenie.db.elephantsql.com',
+        'USER': 'ijlbrymo',
+        'PASSWORD': 'gXMq7FOjK4JmpNgW_kQ5Azsy0taC1QAY',
+        'PORT': '5432'
+        
     }
 }
 
