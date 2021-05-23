23ef8eecff24eb4d5e8707fd3b98fe9e61410513
RohitGJ <77436246+rohitgj169@users.noreply.github.com>
2021-05-23 09:16:36 -0400

    added initial pahts

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
